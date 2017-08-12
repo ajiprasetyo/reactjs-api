6ce34a41d63f3db7596cb2338fca5c42beab6e0a
ajiprasetyo <ajiprasetyo777@gmail.com>
2017-08-13 06:01:24 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+  
+  IncrementItem = () => {
+    this.setState({clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show});
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.IncrementItem}>Cick to IncrementItem by 1</button>
+        <button onClick={this.DecreaseItem}>Cick to IncrementItem by 1</button>
+        <button onClick={this.ToggleClick}>
+          { this.state.show ? 'Hide number' : 'Show number' }
+        </button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+      </div>
+    );
+  }
+}
+
+export default App;
